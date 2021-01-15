e694cdbb4b5377dd1b651fe876e5d1a19e393bf7
Randel M <Randel.C.Moore@gmail.com>
2021-01-15 10:38:35 -0800

    add Identity services to code

--- SweetAndSavory/Startup.cs
+++ SweetAndSavory/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SweetAndSavory.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace SweetAndSavory
 {
@@ -27,6 +28,10 @@ namespace SweetAndSavory
       services.AddEntityFrameworkMySql()
           .AddDbContext<SweetAndSavoryContext>(options => options
           .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
+
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+          .AddEntityFrameworkStores<SweetAndSavoryContext>()
+          .AddDefaultTokenProviders();
     }
 
     public void Configure(IApplicationBuilder app)
@@ -34,6 +39,8 @@ namespace SweetAndSavory
       app.UseStaticFiles();
 
       app.UseDeveloperExceptionPage();
+
+      app.UseAuthentication();
 
       app.UseMvc(routes =>
       {
